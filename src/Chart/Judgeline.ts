3d0a55632cf991b0c74eb45cb162e2c24eaf6c57
Misa Liu <misaliu@misaliu.top>
2025-01-05 18:45:44 +0800

    feat(Chart): Break codes

--- src/Chart/Judgeline.ts
+++ src/Chart/Judgeline.ts
@@ -1,15 +1,7 @@
-import ChartKeyframe from './Keyframe';
-import ChartNote from './Note';
-
-class JudgelineProps {
-  speed: ChartKeyframe[] = [];
-  positionX: ChartKeyframe[] = [];
-  positionY: ChartKeyframe[] = [];
-  alpha: ChartKeyframe[] = [];
-  rotate: ChartKeyframe[] = [];
-}
+import JudgelineProps from './JudgelineProps';
+import Note from './Note';
 
 export default class ChartJudgeline {
   props = new JudgelineProps();
-  notes: ChartNote[] = [];
+  notes: Note[] = [];
 }
---
+++ src/Chart/JudgelineProps.ts
@@ -0,0 +1,9 @@
+import ChartKeyframe from './Keyframe';
+
+export default class ChartJudgelineProps {
+  speed: ChartKeyframe[] = [];
+  positionX: ChartKeyframe[] = [];
+  positionY: ChartKeyframe[] = [];
+  alpha: ChartKeyframe[] = [];
+  rotate: ChartKeyframe[] = [];
+}
