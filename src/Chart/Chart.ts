5c8af03e9b4d3d110b104e4dcd5c90cba01594f6
Misa Liu <misaliu@misaliu.top>
2025-01-06 15:25:18 +0800

    feat(Chart): Add chart info

--- src/Chart/Chart.ts
+++ src/Chart/Chart.ts
@@ -1,9 +1,20 @@
 import ChartBPM from './BPM';
 import ChartJudgeline from './Judgeline';
 import ChartNote from './Note';
+import { ChartInfo } from './types';
 
 export default class Chart {
+  info: ChartInfo;
+  audio: File;
+  background: File;
+
   bpm: ChartBPM[] = [ new ChartBPM([0, 0, 1], 120) ];
   lines: ChartJudgeline[] = [];
   notes: ChartNote[] = [];
+
+  constructor(info: ChartInfo, audio: File, background: File) {
+    this.info = info;
+    this.audio = audio;
+    this.background = background;
+  }
 }
--- src/Chart/types.ts
+++ src/Chart/types.ts
@@ -1,4 +1,12 @@
 
+export type ChartInfo = {
+  name: string,
+  artist: string,
+  illustration: string,
+  level: string,
+  designer: string,
+};
+
 export enum NoteType {
   TAP = 1,
   DRAG = 2,
