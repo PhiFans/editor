3992e392d4a05c3e7a1aa61d6ee85f69f73fe495
Misa Liu <misaliu@misaliu.top>
2025-01-08 18:20:36 +0800

    feat(ui/Timeline): Add standard list & item

---
+++ src/ui/Timeline/List/Item.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { setCSSProperties } from '@/utils/ui';
+
+export type TimelineListItemProps = {
+  height?: number | string,
+  className?: string,
+  style?: React.CSSProperties,
+  children?: JSX.Element,
+};
+
+const TimelineListItem: React.FC<TimelineListItemProps> = ({
+  height,
+  className,
+  style,
+  children
+}: TimelineListItemProps) => {
+  let styles: React.CSSProperties = {};
+  if (style) styles = { ...style };
+  if (height) styles = { ...styles, ...setCSSProperties({ '--height': `${height}` }) };
+
+  return <div
+    className={`timeline-list-item ${className ? className : ''}`}
+    style={styles}
+  >{children}</div>
+};
+
+export default React.memo(TimelineListItem);
---
+++ src/ui/Timeline/List/List.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { setCSSProperties } from '@/utils/ui';
+
+export type TimelineListProps = {
+  itemHeight?: number | string,
+  className?: string,
+  style?: React.CSSProperties,
+  children?: JSX.Element,
+};
+
+const TimelineList: React.FC<TimelineListProps> = ({
+  itemHeight,
+  className,
+  style,
+  children
+}) => {
+  let styles: React.CSSProperties = {};
+  if (style) styles = { ...style };
+  if (itemHeight) styles = { ...styles, ...setCSSProperties({ '--height': `${itemHeight}` }) };
+
+  return <div
+    className={`timeline-list ${className ? className : ''}`}
+    style={styles}
+  >{children}</div>
+};
+
+export default TimelineList;
