f3ff2a95a8698a55a6a8f2baa0b63d5be8904916
Misa Liu <misaliu@misaliu.top>
2025-01-14 23:09:53 +0800

    feat(ui/EditPanel): Add number component

---
+++ src/ui/EditPanel/Number.tsx
@@ -0,0 +1,36 @@
+import Container from './ItemContainer';
+import Input from './Input';
+import { PanelItemPropsBase } from "./types";
+
+type InputProps = PanelItemPropsBase<number> & {
+  min?: number,
+  max?: number,
+  step?: number,
+  placeholder?: number,
+};
+
+const EditPanelNumber = ({
+  label,
+  onChanged,
+  defaultValue,
+  min,
+  max,
+  step,
+  placeholder,
+}: InputProps) => {
+  return (
+    <Container title={label}>
+      <Input
+        type="number"
+        min={min}
+        max={max}
+        step={step}
+        defaultValue={defaultValue}
+        placeholder={`${placeholder ?? ''}`}
+        onChanged={onChanged}
+      />
+    </Container>
+  )
+};
+
+export default EditPanelNumber;
