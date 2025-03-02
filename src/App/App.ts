d41adb2b546813b3d77a291d7a1a03aed9622e0c
Misa Liu <misaliu@misaliu.top>
2025-03-03 01:23:10 +0800

    refactor(App): Remove some props

--- src/App/App.ts
+++ src/App/App.ts
@@ -9,43 +9,11 @@ import { Ticker } from "pixi.js";
 import { ImportChart } from "@/utils/file";
 
 export class App {
-  readonly chartTicker: Ticker = new Ticker();
   readonly audio: AudioClass = Audio;
   readonly assets: AssetsManager = Assets;
   readonly events = new EventEmitter();
   readonly hotkeys = new Hotkeys(this.events);
   private currentChart: Nullable<Chart> = null;
-
-  constructor() {
-    this.chartTicker.start();
-  }
-
-  createChart(info: ChartInfo, audio: File, background: File, applyToApp: boolean = false) {
-    const newChart = new Chart(info, audio, background);
-    if (applyToApp) {
-      if (this.currentChart) this.chartTicker.remove(this.currentChart.tick);
-
-      this.currentChart = newChart;
-      this.chartTicker.add(newChart.tick);
-      this.events.emit('chart.set', newChart);
-    }
-    return newChart;
-  }
-
-  loadChart(chart: ChartExported, audio: File, background: File) {
-    const newChart = ImportChart(chart, audio, background);
-
-    if (this.currentChart) this.chartTicker.remove(this.currentChart.tick);
-    this.currentChart = newChart;
-    this.chartTicker.add(newChart.tick);
-    this.events.emit('chart.set', newChart);
-
-    return newChart;
-  }
-
-  get chart() {
-    return this.currentChart;
-  }
 }
 
 const app = new App();
